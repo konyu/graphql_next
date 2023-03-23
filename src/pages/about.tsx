752bb4cf8625956e7b7494b3fb91ed5fbba6542f
kon_yu <yusukedrive@gmail.com>
2023-03-23 11:21:53 +0900

    Initial commit

---
+++ src/pages/about.tsx
@@ -0,0 +1,14 @@
+const About = () => {
+  const greeting = "hello";
+
+  console.log("foo");
+
+  return (
+    <>
+      <h1>About me</h1>
+      <h2>{greeting}</h2>
+    </>
+  );
+};
+
+export default About;
